3b7ab8637d2b5d9f8196f458324dceada6f366c2
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-04-14 16:47:11 +0500

    :white_check_mark: Added test for fizzbuzz method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -17,10 +17,30 @@ describe Solver do
       expect { @solver.factorial(-1) }.to raise_error(ArgumentError, 'Negative numbers are not allowed')
     end
   end
+  
   context 'testing the reverse_string method' do
     it 'should return reverse string' do
       reverse = @solver.reverse_string('hello')
       expect(reverse).to eq 'olleh'
+    end
+  end
+
+  context 'testing the fizzbuzz method' do
+    it "should return fizz if input is divisible by 3" do
+      fizz = @solver.fizzbuzz(3)
+      expect(fizz).to eq 'fizz'
+    end
+    it "should return buzz if input is divisible by 5" do
+      buzz = @solver.fizzbuzz(5)
+      expect(buzz).to eq 'buzz'
+    end
+    it "should return fizzbuzz if input is divisible by 3 & 5" do
+      fizzbuzz = @solver.fizzbuzz(15)
+      expect(fizzbuzz).to eq 'fizzbuzz'
+    end
+    it "should return number if input is not divisible by 3 or 5" do
+      number = @solver.fizzbuzz(7)
+      expect(number).to eq "#{number}"
     end
   end
 end
