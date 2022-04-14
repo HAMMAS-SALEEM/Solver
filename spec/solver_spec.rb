a7faa370b8c8ef82ced930d4588327f65fdff431
AdornChoga <adornchoga@gmail.com>
2022-04-14 13:00:59 +0200

    Write test of factorial method

---
+++ spec/solver_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe Solver do
+  before :each do
+    @solver = Solver.new
+  end
+  context 'testing the factorial method' do
+    it 'returns factorial if input is 0' do
+      factorial = @solver.factorial(0)
+      expect(factorial).to eq 1
+    end
+    it 'returns factorial if input is positive' do
+      factorial = @solver.factorial(3)
+      expect(factorial).to eq 6
+    end
+    it 'raises an error if input is negative' do
+      factorial = @solver.factorial(-1)
+      expect(factorial).to raise_error('Negative numbers are not allowed')
+    end
+  end
+end
