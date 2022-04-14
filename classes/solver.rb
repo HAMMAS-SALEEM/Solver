846129657daf78bf1472d95f1497d02b3cb7dca6
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-04-14 16:25:51 +0500

    :bug: Fixed linter errors

--- classes/solver.rb
+++ classes/solver.rb
@@ -1,11 +1,8 @@
 class Solver
   def factorial(num, product = 1)
-    if num < 0
-      raise ArgumentError, "Negative numbers are not allowed"
-    end
-    if num == 1 || num == 0
-      return product
-    end
-    return factorial(num - 1, product * num)
+    raise ArgumentError, 'Negative numbers are not allowed' if num.negative?
+    return product if [1, 0].include?(num)
+
+    factorial(num - 1, product * num)
   end
 end
